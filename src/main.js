e17a02bdf9b20216ed22435aa67e1dc3e30d5848
Cecilia de la Parra <delaparracecilia@gmail.com>
2022-09-22 19:12:37 +0200

    color palette

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 import App from './App.vue';
+import style from './assets/style.css'
 
 import router from './router';
 
